49186d592fe350ece6c5cb525ce40c536065cc0a
ihapmustapha <ihapmustapha@gmail.com>
2019-10-25 22:20:36 +0200

    Linear Search

---
+++ src/Algorithms/Linear Search.js
@@ -0,0 +1,10 @@
+function search(array, seekElement) {
+    let found = [];
+    array.forEach((element, index) => {
+        if(element === seekElement) found.push({ index: index, value: element })    
+    });
+    return found;
+}
+
+/************Tests***************/
+console.log(...search([2, 5, 87, 23, 13, 7, 87], 87));
