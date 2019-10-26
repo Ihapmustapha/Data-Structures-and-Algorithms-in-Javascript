0e3eff5c9ab605d0feee73b00e14323d7b13f25a
ihapmustapha <ihapmustapha@gmail.com>
2019-10-26 21:47:50 +0200

    Insertion Sort

---
+++ src/Algorithms/Sort/Insertion Sort.js
@@ -0,0 +1,24 @@
+function sort(array) {
+    for(let i = 1; i < array.length; i++) {
+        let extractedElement = array[i];
+        for(var j = i-1; j >= 0 && array[j] > extractedElement; j--) {
+            array[j+1] = array[j];
+        }
+        array[j+1] = extractedElement;
+
+        // for tracing: 
+        // console.log("at iteration: " + i);
+        // console.log(array);
+    }
+    return array;
+}
+
+
+/***************Tests***************/
+console.log(sort([2,69,5,12,0,7,44,120,3,98]));
+
+
+
+
+
+
