82a7d27f3b9b5f10ba639d6ca18bdcfca1730f48
ihapmustapha <ihapmustapha@gmail.com>
2019-10-27 11:00:27 +0200

    linked list functions

--- src/Data Structures/Linked List/Linked List.js
+++ src/Data Structures/Linked List/Linked List.js
@@ -31,32 +31,99 @@ class LinkedList {
     }
     // Searchs and remove the element from the list
     remove(element) {
-        let currentNode = head; 
-        if(element === currentNode.value) {}
+        var currentNode = head;
+        var previousNode;
+        if(currentNode.element === element){
+            head = currentNode.next;
+        } else {
+        while(currentNode.element !== element) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        previousNode.next = currentNode.next;
+    }
+
+        length --;
     }
     // checks if linked list is empty
     isEmpty() {
-
+        return length === 0;
     }
     // returns index of item
     indexOf() {
+        var currentNode = head;
+    var index = -1;
 
+    while(currentNode){
+        index++;
+        if(currentNode.element === element){
+            return index;
+        }
+        currentNode = currentNode.next;
     }
+
+    return -1;
+  };
     // returns element at specific index
     elementAt() {
-
+        var currentNode = head;
+    var count = 0;
+    while (count < index){
+        count ++;
+        currentNode = currentNode.next
+    }
+    return currentNode.element;
     }
     // add element at specific index
     addAt() {
+        var node = new Node(element);
 
+    var currentNode = head;
+    var previousNode;
+    var currentIndex = 0;
+
+    if(index > length){
+        return false;
+    }
+
+    if(index === 0){
+        node.next = currentNode;
+        head = node;
+    } else {
+        while(currentIndex < index){
+            currentIndex++;
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        node.next = currentNode;
+        previousNode.next = node;
+    }
+    length++;
     }
     // removes element at specific index
     removeAt() {
-
+        var currentNode = head;
+    var previousNode;
+    var currentIndex = 0;
+    if (index < 0 || index >= length){
+        return null
+    }
+    if(index === 0){
+        head = currentNode.next;
+    } else {
+        while(currentIndex < index) {
+            currentIndex ++;
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        previousNode.next = currentNode.next
+    }
+    length--;
+    return currentNode.element;
+  }
     }
 
-
-}
 
 /********************Tests*************************/
 let conga = new LinkedList();
