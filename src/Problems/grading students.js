4b1ba62e1f064861586f7a462439e2b173b26bdc
ihapmustapha <ihapmustapha@gmail.com>
2019-11-10 19:53:48 +0200

    grading students problem

---
+++ src/Problems/grading students.js
@@ -0,0 +1,18 @@
+/*
+ * https://www.hackerrank.com/challenges/grading/problem
+ */
+
+const gradingStudents = grades => {
+  let roundedGrades = [];
+  for (let i = 0; i < grades.length; i++) {
+    if (grades[i] > 37 && 5 - (grades[i] % 5) <= 3) {
+      roundedGrades.push(grades[i] + 5 - (grades[i] % 5));
+    } else {
+      roundedGrades.push(grades[i]);
+    }
+  }
+  return roundedGrades;
+};
+
+/***********Tests************/
+console.log(gradingStudents([73, 67, 38, 33]));
